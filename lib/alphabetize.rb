96011c2d48279daa351360c17a12f8b1acf981c8
Jordan R <jordan.rivera34@gmail.com>
2019-03-09 01:53:59 -0500

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
 def alphabetize(arr)
   # code here
+  # assign the letter index to a variable
+  ea = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+  # go through the array and sort the words
+  arr.sort_by do |words|
+    # split the words by letter and check them
+    words.split('').collect do |x|
+      # cross reference the letter via the letter index up top
+      ea.index(x)
+    end
+  end
 end
